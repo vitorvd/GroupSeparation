1fa2fa9386618d7ea3c0a39a5b8a7f39702bbb96
Vitor Versiani <vitordversiani@gmail.com>
2021-03-22 17:32:24 -0300

    atualizações no "participantsLimit"

--- group.py
+++ group.py
@@ -2,14 +2,15 @@ from participant import Participant
 
 
 class Group:
+    participantsLimit = 6
+
     def __init__(self, tag):
         self.tag = tag
         self.participants = []
 
     def addParticipant(self, name, contact, experience):
-        if len(self.participants) < 6:
+        nonlocal participantsLimit
+        if len(self.participants) < participantsLimit:
             self.participants.append(Participant(name, contact, experience))
-            # print(f'**ADICIONADO** {nome} adicionado ao Grupo {self.tag}')
             return True
-        # print(f'**ERRO** ao cadastrar {nome} no Grupo {self.tag} que possui {len(self.participantes)} participantes')
         return False
